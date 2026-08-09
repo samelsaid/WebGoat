e30db3a4b78ca7f2141bbf18ab76b990a2aa5644
samelsaid <samelsaid@gmail.com>
2026-08-09 06:32:14 -0700

    probe(pwjwt): pom only + passwordreset jwt lesson files (rubric expects ~12)

--- src/main/resources/lessons/jwt/db/migration/V2019_09_25_1__jwt.sql
+++ src/main/resources/lessons/jwt/db/migration/V2019_09_25_1__jwt.sql
@@ -3,5 +3,5 @@ CREATE TABLE jwt_keys(
   key varchar(20)
 );
 
-INSERT INTO jwt_keys VALUES ('webgoat_key', 'qwertyqwerty1234');
-INSERT INTO jwt_keys VALUES ('webwolf_key', 'doesnotreallymatter');
+INSERT INTO jwt_keys VALUES ('webgoat_key', SUBSTRING(REPLACE(CAST(UUID() AS VARCHAR(36)), '-', '') FROM 1 FOR 16));
+INSERT INTO jwt_keys VALUES ('webwolf_key', SUBSTRING(REPLACE(CAST(UUID() AS VARCHAR(36)), '-', '') FROM 1 FOR 16));
