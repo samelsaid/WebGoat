c92f56dc7f832a096e8d2f67002a9b9a5fe9915e
samelsaid <samelsaid@gmail.com>
2026-08-09 08:15:55 -0700

    probe: exempt password-reset reset-link endpoints from the CSRF token

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -71,7 +71,10 @@ public class WebSecurityConfig {
                 csrf.csrfTokenRepository(csrfTokenRepository)
                     .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
                     .ignoringRequestMatchers(
-                        CsrfExemptions.headerlessAuthentication("/login", "/register.mvc")))
+                        CsrfExemptions.headerlessAuthentication("/login", "/register.mvc"),
+                        CsrfExemptions.anyRequestTo("/PasswordReset/ForgotPassword/create-password-reset-link",
+                            "/PasswordReset/reset/change-password",
+                            "/PasswordReset/reset/login")))
         .addFilterAfter(new CsrfTokenCookieFilter(), CsrfFilter.class)
         .exceptionHandling(
             handling ->
